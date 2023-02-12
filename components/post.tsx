552c1bb0df89bdf2eae0c9b6b51e7c5b68924440
gugisek <gugisek@gmail.com>
2023-02-12 22:55:28 +0100

    Add carusel and redesign buttons in post

--- components/post.tsx
+++ components/post.tsx
@@ -8,41 +8,85 @@ type Props = {
     description: string,
     link: string,
     github: string,
-    image: string,
+    image1: string,
+    image2: string,
+    image3: string,
     technologies: string
 }
 
-export default function post({name, description, link, github, image, technologies}: Props) {
+export default function post({name, description, link, github, image1, image2, image3, technologies}: Props) {
+
+  const [slideActive, setSlideActive] = React.useState(0)
+
   return (
     <motion.div 
     initial={{opacity: 0, x: 100}}
     whileInView={{opacity: 1, x: 0}}
     transition={{duration: 1}}
     className='flex flex-col items-center justify-center w-full'>
-            <div className='flex xl:flex-row flex-col md:w-3/5 sm:w-2/3 w-full sm:px-0 px-6 gap-10 items-center justify-center'>
-              <div className='xl:w-2/3 md:w-2/3 w-full flex items-center xl:justify-end justify-center'><img src={image} alt={name + " image"} className='shadow-xl rounded-xl max-w-[350px]'/></div>
-              <div className='flex flex-col justify-center xl:w-3/4 md:w-2/3 w-full'>
-                <h1 className='font-[Lexend-bold] text-3xl pb-6'>{name}</h1>
+            <div className='flex xl:flex-row flex-col md:w-3/5 sm:w-2/3 w-full sm:px-0 px-6 gap-10 items-center justify-center mt-3'>
+              <div className='xl:w-4/5 md:w-2/3 w-full max-w-[350px] flex flex-col items-center xl:justify-end justify-center'>
+                <div className='flex items-center justify-center'>
+                  <img id="slides" src={image1} alt={name + " image"} className={`${slideActive==0 ? 'opacity-100' : 'opacity-0 absolute'} shadow-xl rounded-xl max-w-[350px] z-0`}/>
+                  <img id="slides" src={image2} alt={name + " image"} className={`${slideActive==1 ? 'opacity-100' : 'opacity-0 absolute'} shadow-xl rounded-xl max-w-[350px] z-0`}/>
+                  <img id="slides" src={image3} alt={name + " image"} className={`${slideActive==2 ? 'opacity-100' : 'opacity-0 absolute'} shadow-xl rounded-xl max-w-[350px] z-0`}/>
+                </div>
+                <div className='flex felx-row z-10 mt-[-5px]'>
+                  <div>
+                    <button onClick={() => setSlideActive(0)} className="py-3" >
+                      <div className={`${slideActive==0 ? 'bg-[#4d4d4d]' : 'bg-[#9c9c9c]'} w-6 h-[3px] rounded-full mx-1 transition-all duration-300`}></div>
+                    </button>
+                  </div>
+                  <div>
+                    <button onClick={() => setSlideActive(1)} className="py-3">
+                      <div className={`${slideActive==1 ? 'bg-[#4d4d4d]' : 'bg-[#9c9c9c]'} w-6 h-[3px] rounded-full mx-1 transition-all duration-300`}></div>
+                    </button>
+                  </div>
+                  <div>
+                    <button onClick={() => setSlideActive(2)} className="py-3">
+                      <div className={`${slideActive==2 ? 'bg-[#4d4d4d]' : 'bg-[#9c9c9c]'} w-6 h-[3px] rounded-full mx-1 transition-all duration-300`}></div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className='flex flex-col justify-center xl:w-2/4 md:w-2/3 w-full mt-[-15px]'>
+                <h1 className='font-[Lexend-bold] text-3xl pb-3 flex items-center justify-between'>
+                  {name}
+                  <span className='flex flex-row'>
+                    {link && (
+                    <a href={link} className="btnMini flex items-center justify-center p-2 mx-2" target='_blank'>
+                      <img src="website.png" alt="website icon" className='w-[20px] h-[20px]'/>
+                    </a>
+                    )}
+                    {github && (
+                    <a href={github} className="btnMini flex items-center justify-center p-2 mx-2" target='_blank'>
+                      <img src="github.png" alt="github icon" className='w-[20px] h-[20px]'/>
+                    </a>
+                    )}
+                  </span>
+                </h1>
                 <div className='font-[Lexend]'>
                   <span dangerouslySetInnerHTML={{__html: description}}></span>
                 </div>
-              </div>
-            </div>
-            <div className='flex flex-row gap-4 mt-3'>
+                {/* <div className='flex flex-row gap-4 my-2'>
                   {link && (
-                  <a href={link} className="btn">
+                  <a href={link} className="btn" target='_blank'>
                     website
                     <img src="website.png" alt="website icon" />
                   </a>
                   )}
                   {github && (
-                  <a href={github} className="btn">
+                  <a href={github} className="btn" target='_blank'>
                     github
                     <img src="github.png" alt="github icon" className='w-[20px] h-[20px]'/>
                   </a>
                   )}
-                </div>
-            <p className='uppercase text-center py-7 sm:px-0 px-6 text-sm font-[Lexend] text-[#656565]' style={{wordSpacing: '15px'}}>{technologies}</p>
+                </div> */}
+              </div>
+              
+            </div>
+            
+            <p className='uppercase text-center pb-7 pt-1 sm:px-0 px-6 text-sm font-[Lexend] text-[#656565]' style={{wordSpacing: '15px'}}>{technologies}</p>
           </motion.div>
   )
 }
--- components/works.tsx
+++ components/works.tsx
@@ -13,8 +13,10 @@ export default function works({}: Props) {
     name: 'RGBpc.pl',
     description: "<p>Gaming shop fully customized <span style='color: #7b02ff'>woocomerce</span> shematic</p>                <li>fully working product pages</li>                <li>avaible variants</li>                <li>zooming photos</li>                <li>modernistic arhive style </li>            <p>                Promo codes and direct pay via <span style='color: #3013E1'>Gpay</span> and <span style='color: #000'>Apple Pay</span>                <br>                Working paynaments via <span style='color: #35A5E4'>PayPal</span> and <span style='color: #BE06FF'>Stripe</span>                <br>                Different shipping methods even <span style='color: #C99612'>InPost Paczkomat</span>           </p>",
     link: 'https://www.rgbpc.pl',
-    github: '',
-    image: 'rgbpc.png',
+    github: 'es',
+    image1: 'rgbpc.png',
+    image2: 'rgbpc2.png',
+    image3: 'rgbpc3.png',
     technologies: 'HTML CSS JS PHP MySQL Wordpress Linux'
   },
   {
@@ -22,7 +24,9 @@ export default function works({}: Props) {
     description: "<p>                Website with <span style='color: #d800ff'>tutorials</span> for practical exams for IT.            </p>                <li>working search bar</li>                <li>sorting elements by date</li>                <li>download files from server</li>            <p>                Builded on subsites and folder with elements as objects in list on site.            </p>",
     link: 'https://www.praktyczny-informatyk.pl',
     github: '',
-    image: 'praktyczny-informatyk.pl.png',
+    image1: 'praktyczny-informatyk.pl.png',
+    image2: 'praktyczny-informatyk.pl2.png',
+    image3: 'praktyczny-informatyk.pl3.png',
     technologies: 'HTML CSS JS PHP'
   },
   {
@@ -30,7 +34,9 @@ export default function works({}: Props) {
     description: "<p>                App for <span style='color: #ff0000'>party</span> organization in local area.            </p>                <li>fully operative prototype</li>                <li>variants for buttons</li>                <li>projected messenging function</li>                <li>transition animations</li>            <p>                Builded for easy and fast finding new people to party with. Only prototype.            </p>",
     link: '',
     github: '',
-    image: 'partyhub.png',
+    image1: 'partyhub.png',
+    image2: 'partyhub2.png',
+    image3: 'partyhub3.png',
     technologies: 'figma'
   },
   {
@@ -38,19 +44,21 @@ export default function works({}: Props) {
     description: '<p>                Website for tech <span style="color: #0097ff">agency</span> with <span style="color: #0097ff">IT</span> services.            </p>                <li>fully operative prototype</li>                <li>variants for buttons</li>                <li>projected customer panel with diffrent sections</li>                <li>widgets in client panel</li>                <li>parallax scrolling efects on sections in home page</li>            <p>                Projected for agency company as platform for menaging services for customers. On this moment only prototype.            </p>',
     link: '',
     github: '',
-    image: 'agencytech.pl.png',
+    image1: 'agencytech.pl.png',
+    image2: 'agencytech.pl2.png',
+    image3: 'agencytech.pl3.png',
     technologies: 'figma'
   }
   ]
   return (
-    <section id="my-works" className='min-h-screen text-neutral-900 flex flex-col items-center justify-start'>
+    <section id="my-works" className='min-h-screen text-neutral-900 flex flex-col items-center justify-start pb-7'>
         <motion.h1 
         initial={{opacity: 0, y: 100}}
         whileInView={{opacity: 1, y: 0}}
         transition={{duration: 0.5}}
         className='uppercase font-[Lexend-bold] text-5xl text-[#e0e0e0] py-16'>My projects</motion.h1>
         {projects.map((project, index) => (
-          <Post key={index} name={project.name} description={project.description} link={project.link} github={project.github} image={project.image} technologies={project.technologies}/>
+          <Post key={index} name={project.name} description={project.description} link={project.link} github={project.github} image1={project.image1} image2={project.image2} image3={project.image3} technologies={project.technologies}/>
         ))}
 
     </section>
